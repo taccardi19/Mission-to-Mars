c8035dfd36beffa65a8db4f9beaea13db33827bc
Ted Accardi <94876635+taccardi19@users.noreply.github.com>
2022-02-25 23:56:53 -0500

    Adding all module files

---
+++ Apps/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scraping
+
+#Set up flask
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+#Define the route for HTML page
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+#Set up scraping route
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update_one({}, {"$set":mars_data}, upsert=True)
+   return redirect('/', code=302)
+
+#Tell flask to run
+if __name__ == "__main__":
+   app.run()
+
